95c46b8db43813b2c3826c4b172702310f19a454
somukhan <mhsomrat154@gmail.com>
2022-10-05 17:13:07 +0600

    third commit

--- src/components/Form.js
+++ src/components/Form.js
@@ -64,6 +64,12 @@ function Form() {
   const handleSubmit = (e) => {
     e.preventDefault()
     console.log(`Form Submitted Successfully.`)
+    setFormUser({
+      name: '',
+      email: '',
+      password: '',
+      confirmPassword: '',
+    })
   }
 
   return (
